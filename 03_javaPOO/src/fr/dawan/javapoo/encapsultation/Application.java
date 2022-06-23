6f0ed3969ebfd9b881499a440dfc899da2d06fec
Woodson Juste <j.woodson@hotmail.fr>
2022-06-23 09:55:26 +0200

    initial commit (baseJava, debut POO)

---
+++ 03_javaPOO/src/fr/dawan/javapoo/encapsultation/Application.java
@@ -0,0 +1,18 @@
+package fr.dawan.javapoo.encapsultation;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Utilisateur user3 = new Utilisateur("Rudd", "Emily", 27);
+		System.out.println("AgeUser3 : " + user3.getAge());
+		
+		user3.setAge(12);
+		
+		System.out.println("Age Emily : " + user3.getAge() + " ans");
+		
+		System.out.println("Nombre d'utilisateur : " + Utilisateur.getNbreUtilisateur());
+		
+	}
+
+}
